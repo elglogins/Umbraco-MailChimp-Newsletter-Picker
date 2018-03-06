d73e26b721e88ce2d1016cbc1107dff3b2b5cd50
Elgars Logins <elgars@siteocean.dk>
2018-03-06 17:05:41 +0100

    Basic implementation of property editor

---
+++ App_Plugins/SiteOceanMailChimpListPicker/editor.controller.js
@@ -0,0 +1,14 @@
+﻿angular.module("umbraco")
+    .controller("SiteOcean.MailChimp.ListPickerController", function ($scope, $http, notificationsService) {
+
+        $scope.model.lists = [];
+        $scope.model.status = 'loading';
+
+        $http.get('/umbraco/backoffice/api/mailchimpnewsletter/lists').then(function (response) {
+            $scope.model.lists = response.data.lists;
+            $scope.model.status = 'ready';
+        }, function(error) {
+            $scope.model.status = 'error';
+            notificationsService.error("Couldn't load MailChimps newsletters list");
+        });
+});
---
+++ Controllers/MailChimpNewsletterController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
+using Umbraco.Core;
+using Umbraco.Core.Services;
+using Umbraco.Web.Editors;
+
+namespace FreshUmbraco.Controllers
+{
+    public class MailChimpNewsletterController : UmbracoAuthorizedJsonController
+    {
+        private readonly IDataTypeService _dataTypeService;
+
+        public MailChimpNewsletterController()
+        {
+            _dataTypeService = ApplicationContext.Current.Services.DataTypeService;
+        }
+
+        [System.Web.Http.HttpGet]
+        public object Lists()
+        {
+            var apiKey = GetMailChimpApiKeyValue();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("https://{0}.api.mailchimp.com/3.0/lists", GetMailChimpDataCenterIdentifier(apiKey)));
+            request.ContentType = "application/json; charset=utf-8";
+            request.Headers["Authorization"] = "apikey " + apiKey;
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return JsonConvert.DeserializeObject(reader.ReadToEnd());
+            }
+        }
+
+        private string GetMailChimpDataCenterIdentifier(string apiKey)
+        {
+            return apiKey.Substring(apiKey.LastIndexOf('-') + 1);
+        }
+
+        private string GetMailChimpApiKeyValue()
+        {
+            var dataType = _dataTypeService.GetDataTypeDefinitionByPropertyEditorAlias("SiteOcean.MailChimp.ListPicker").First();
+            var prevalues = _dataTypeService.GetPreValuesCollectionByDataTypeId(dataType.Id);
+            return prevalues.PreValuesAsDictionary["apiKey"].Value;
+        }
+    }
+}
